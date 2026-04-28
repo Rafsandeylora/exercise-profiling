8dc4a84562c19147bf69fd7bbcb66359f8c570ec
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-04-28 16:36:11 +0700

    [Refactoring] - Optimize getAllStudentName using projection query

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -11,5 +11,6 @@ import java.util.List;
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
+    @Query("SELECT s.name FROM Student s")
+    List<String> findAllStudentNames();
 }
